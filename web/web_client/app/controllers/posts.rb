87539c0dcebe616c28276516c58593086a39c4cd
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-18 15:24:32 -0400

    add simple web layer boilerplate

---
+++ web/web_client/app/controllers/posts.rb
@@ -0,0 +1,13 @@
+WebClient.controllers :posts do
+
+  get :index do
+      @posts = Post.all(:order => 'created_at desc')
+      render 'posts/index'
+  end
+
+  get :show, :with => :id do
+      @post = Post.find_by_id(params[:id])
+      render 'posts/show'
+  end
+
+end
